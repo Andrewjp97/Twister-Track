1f29d2dc87051ef93a1fe6bb6dfe2ac7ef2f59f3
Andrew Paterson <AndrewPaterson@andrewpaterson.sytes.net>
2013-11-06 20:28:14 -0500

    getting more comfortable with XCTest

--- Twister TrackTests/Twister_TrackTests.m
+++ Twister TrackTests/Twister_TrackTests.m
@@ -39,5 +39,45 @@
         XCTFail(@"Array does not have HI or HELLO in it, instead it has %@ and %@", _array[0], _array[1]);
     }
 }
-
+- (void) testReturn {
+    int i = 0;
+    for (i = 0; i <= 100; i++) {
+        int point = i;
+        NSString *string = [self returnStringBasedOnPoint:point];
+        if ([string isEqualToString:@"First"] && (point >= 0 && point <=24)) {
+            return;
+        }
+        else if([string isEqualToString:@"Second"] && (point >= 25 && point <= 49)){
+            return;
+        }
+        else if ([string isEqualToString:@"Third"] && (point >= 50 && point <= 74)){
+            return;
+        }
+        else if ([string isEqualToString:@"Fourth"] && (point >=75 && point <= 100)){
+            return;
+        }
+        else {
+            XCTFail(@"The returned String %@ is not within the correct quadrant for a point with value %u", string, point);
+        }
+    }
+    
+}
+- (NSString *)returnStringBasedOnPoint:(int)point{
+    if (point < 25) {
+        return @"First";
+    }
+    else if (point < 50){
+        return @"Second";
+    }
+    else if (point < 75){
+        return @"Third";
+    }
+    else if (point < 101){
+        return @"Fourth";
+    }
+    else{
+        XCTFail(@"Outside the acceptable range");
+        return nil;
+    }
+}
 @end
