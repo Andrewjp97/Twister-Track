92ece61a8b3b58bbed43a1bb529d79a572967a12
Andrew Paterson <AndrewPaterson@andrewpaterson.sytes.net>
2013-11-11 06:35:53 -0500

    commit

--- Twister Track/MyScene.m
+++ Twister Track/MyScene.m
@@ -7,48 +7,61 @@
 //
 
 #import "MyScene.h"
+#import <CoreMotion/CoreMotion.h>
+@interface MyScene ()
+@property (strong) CMMotionManager* motionManager;
 
+@end
 @implementation MyScene
-
+#define kShipSize CGSizeMake(30, 16)
+#define kShipName @"ship"
 -(id)initWithSize:(CGSize)size {    
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
-        
-        self.backgroundColor = [SKColor colorWithRed:0.32 green:0.32 blue:0.32 alpha:1.0];
-        
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-        
-        myLabel.text = @"Hello, World!";
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
-        
-        [self addChild:myLabel];
+        self.motionManager = [[CMMotionManager alloc] init];
+        [self.motionManager startAccelerometerUpdates];
+        self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
+       [self setupShip];
     }
     return self;
 }
 
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    /* Called when a touch begins */
-    
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        SKAction *tt = [SKAction scaleTo:0.25 duration:1];
-        [sprite runAction:tt];
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
-    }
-}
+
 
 -(void)update:(CFTimeInterval)currentTime {
     /* Called before each frame is rendered */
+    [self processUserMotionForUpdate:currentTime];
+}
+-(void)setupShip {
+    //1
+    SKNode* ship = [self makeShip];
+    //2
+    ship.position = CGPointMake(self.size.width / 2.0f, kShipSize.height/2.0f);
+    [self addChild:ship];
 }
 
+-(SKNode*)makeShip {
+    SKNode* ship = [SKSpriteNode spriteNodeWithColor:[SKColor greenColor] size:kShipSize];
+    ship.name = kShipName;
+    //1
+    ship.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:ship.frame.size];
+    //2
+    ship.physicsBody.dynamic = YES;
+    //3
+    ship.physicsBody.affectedByGravity = NO;
+    //4
+    ship.physicsBody.mass = 0.02;
+    return ship;
+}
+-(void)processUserMotionForUpdate:(NSTimeInterval)currentTime {
+    //1
+    SKSpriteNode* ship = (SKSpriteNode*)[self childNodeWithName:kShipName];
+    //2
+    CMAccelerometerData* data = self.motionManager.accelerometerData;
+    //3
+    if (fabs(data.acceleration.x) > 0.2) {
+        //4 How do you move the ship?
+       [ship.physicsBody applyForce:CGVectorMake(40.0 * data.acceleration.x, 0)];
+    }
+}
 @end
